3475624b2369dc3cfd2f5efcba0a78aa2f685b5b
skay138 <nine1352@gmail.com>
2022-07-16 16:10:23 +0900

    GG

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 import "./App.css";
 import { useEffect, useState } from "react";
 import { markerList } from "./markerList";
+import pindata from "./pintest";
 
 const { kakao } = window;
 
@@ -62,6 +63,8 @@ function Main() {
   }
 
   useEffect(() => {
+    console.log(pindata);
+
     var mapContainer = document.getElementById("myMap"), // 지도를 표시할 div
       mapOption = {
         center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
@@ -73,14 +76,26 @@ function Main() {
     geolocation(); //geolaction
 
     // 흡연구역 표시
-    markerList.forEach((el) => {
+    pindata.forEach((el) => {
+      var imageSrc = "https://img.icons8.com/material-rounded/96/000000/marker.png", // 마커이미지의 주소입니다
+        imageSize = new kakao.maps.Size(30, 30), // 마커이미지의 크기입니다
+        imageOption = { offset: new kakao.maps.Point(27, 69) }; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+
+      // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
+      var markerImage = new kakao.maps.MarkerImage(
+        imageSrc,
+        imageSize,
+        imageOption
+      );
+
       var marker = new kakao.maps.Marker({
+        image: markerImage,
         map: map,
-        position: new kakao.maps.LatLng(el.lat, el.lon),
+        position: new kakao.maps.LatLng(el.Latitude, el.Longitude),
         title: el.name,
       });
 
-      var iwContent = el.name, // 인포윈도우에 표시할 내용
+      var iwContent = el.Location, // 인포윈도우에 표시할 내용
         iwRemoveable = false;
 
       // 인포윈도우를 생성합니다
@@ -90,13 +105,13 @@ function Main() {
       });
 
       // 인포윈도우를 마커위에 표시합니다
-      infowindow.open(map, marker);
+      //infowindow.open(map, marker);
 
       //마커의 클릭이벤트
-      kakao.maps.event.addListener(marker, 'click', function() {
+      kakao.maps.event.addListener(marker, "click", function () {
         document.getElementById("pininfo").className = "addpin";
+        document.getElementById("pinname").innerHTML = el.Location;
       });
-      
     });
 
     var marker = new kakao.maps.Marker({
@@ -209,7 +224,7 @@ function Main() {
           id="currentlo"
           onClick={geolocation}
         >
-          <span className="material-symbols-outlined">share_location</span>
+          <img src="https://img.icons8.com/sf-black-filled/64/000000/location-update.png" width="50px"/>
         </button>
       </div>
 
@@ -224,10 +239,8 @@ function Main() {
 
       {/* 여기부터는 핀정보입니다 */}
       <div id="pininfo" className="addpin hide">
-        
+        <h3 id="pinname"></h3>
       </div>
-
-
     </div>
   );
 }
