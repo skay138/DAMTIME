880a2563322f4db812d199d0af3d5dde1e4f20c2
Dev_P3PP3R <102704267+DevP3PP3R@users.noreply.github.com>
2022-07-19 14:28:17 +0900

    카메라 진행중
    Addfin 내부에 카메라 버튼

--- src/Addfin.js
+++ src/Addfin.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import Button from "./Button";
+import Camera from "./camera";
 
 function Addpin({lat, lon}) {
 
@@ -9,9 +10,11 @@ function Addpin({lat, lon}) {
   return (
     <div id="addpin" className="info hide">
         <h3>흡연구역을 등록해주세요</h3>
+        <Camera />
         <input type="text" id="pinName" placeholder="장소명 입력"></input>
         <Button name="등록" action="submit"/>
         <Button name="취소" action="close"/>
+        
       </div>
   );
 }
