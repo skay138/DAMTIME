f1b32924569a6e6f0830be530d95b4656a709fca
DevP3PP3R <petercater@naver.com>
2022-07-21 18:29:31 +0900

    11

---
+++ src/admin.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+function Admin () {
+
+   
+
+    return (
+        <div id="admin">
+            <div id="member">
+                { }
+            </div>
+        </div>
+    );
+}
+
+export default Admin
