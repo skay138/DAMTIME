724f9588fd7785515700ff6f27a8d8e00ab4b0ab
DevP3PP3R <petercater@naver.com>
2022-07-28 15:01:16 +0900

    localhost guys

--- src/main.js
+++ src/main.js
@@ -154,21 +154,13 @@ function Main() {
 
   //bagng.asuscomm.com <-> localhost
   useEffect(() => {
-<<<<<<< HEAD
-    axios.get("http://localhost:4000/api").then((res) => {
-=======
-    axios.get("localhost:4000/api").then((res) => {
->>>>>>> 2a1151a035ab5d88b325ca02d2545b97aa68b1de
+    axios.get("https://localhost:4000/api").then((res) => {
       setPindata(res.data);
     }).catch(error => console.log(error));
   }, [refresh]);
 
   useEffect(() => {
-<<<<<<< HEAD
-    axios.get("http://localhost:4000/userpin").then((res) => {
-=======
-    axios.get("localhost:4000/userpin").then((res) => {
->>>>>>> 2a1151a035ab5d88b325ca02d2545b97aa68b1de
+    axios.get("https://localhost:4000/userpin").then((res) => {
       setUserpin(res.data);
     }).catch(error => console.log(error));
   }, [refresh]);
