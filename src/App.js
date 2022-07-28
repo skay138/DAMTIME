3cd7cb21173724f73d6e1a610b9490eaa16bcc7f
URMUSEI <danceyouoff23@gmail.com>
2022-07-28 13:07:01 +0900

    re

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import "./App.css";
 import Main from "./main.js";
+import {Route, Routes, useLocation} from 'react-router-dom';
+import Report from "./reportform";
+import { useState } from "react";
 
 function App() {
+  
   return (
-    <div>
-  <Main />
-  </div>);
+    <Routes>  
+    <Route path="/" element={<Main />} />
+    <Route path="/report/" element={<Report />} /> 
+    </Routes>
+    );
 }
 
 export default App;
