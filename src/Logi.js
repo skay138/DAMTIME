5569be4b6b5787e31afc3cd53e61ead64f20f836
skay138 <nine1352@gmail.com>
2022-08-01 23:14:15 +0900

    test modify

--- src/Logi.js
+++ src/Logi.js
@@ -1,4 +1,6 @@
 import { useEffect, useState } from "react";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
 import "./logi.css";
 
 function EntryPage() {
@@ -8,6 +10,52 @@ function EntryPage() {
     setState({
       currentView: view,
     });
+  };
+
+  //로그인
+  const [inputId, setInputId] = useState("");
+  const [inputPw, setInputPw] = useState("");
+
+  const loginId = {
+    userid: inputId,
+    userpw: inputPw,
+  };
+
+  const navigate = useNavigate();
+
+  let sessionStorage = window.sessionStorage;
+
+  //input data의 변화가 있을때마다 value값 useState화
+  const handleInputId = (e) => {
+    setInputId(e.target.value);
+  };
+
+  const handleInputPw = (e) => {
+    setInputPw(e.target.value);
+  };
+
+  //Login 버튼 클릭 이벤트
+  const onClickLogin = () => {
+    if (loginId.userid === "" || loginId.userpw === "") {
+      alert("아이디 혹은 비밀번호를 기입하지 않으셨습니다.");
+    } else {
+      axios.post("/damlogin", loginId).then(function (res) {
+        if (res.data === true) {
+          navigate("/main");
+          sessionStorage.setItem("loginId", loginId.userid);
+        } else if (res.data === false) {
+          alert("비밀번호가 일치하지 않습니다.");
+        } else {
+          console.log(res);
+          alert(res.data);
+        }
+      });
+    }
+  };
+
+  const nonmember = () => {
+    sessionStorage.setItem("loginId", "non");
+    navigate("/main");
   };
 
   const currentView = () => {
@@ -34,7 +82,11 @@ function EntryPage() {
               </ul>
             </fieldset>
             <button>Submit</button>
-            <button type="button" onClick={() => changeView("logIn")}>
+            <button
+              className="loginbtn"
+              type="button"
+              onClick={() => changeView("logIn")}
+            >
               로그인 페이지로
             </button>
           </form>
@@ -52,12 +104,20 @@ function EntryPage() {
                     type="email"
                     id="userid"
                     placeholder="type your email"
+                    value={inputId}
+                    onChange={handleInputId}
                     required
                   />
                 </li>
                 <li>
                   <label htmlFor="password">Password:</label>
-                  <input type="password" id="password" required />
+                  <input
+                    type="password"
+                    id="password"
+                    required
+                    value={inputPw}
+                    onChange={handleInputPw}
+                  />
                 </li>
                 <li>
                   <i />
@@ -66,10 +126,25 @@ function EntryPage() {
                   </a>
                 </li>
               </ul>
+              <br/>
+              <button
+                className="loginbtn"
+                type="submit"
+                onClick={onClickLogin}
+                value="LOGIN"
+              >
+                Login
+              </button>
             </fieldset>
-            <button>Login</button>
+            <button
+              onClick={nonmember}
+              type="button"
+              value="Enter by Non-member"
+            >
+              Enter by Non-member
+            </button>
             <button type="button" onClick={() => changeView("signUp")}>
-              Create an Account
+              회원가입하기
             </button>
           </form>
         );
