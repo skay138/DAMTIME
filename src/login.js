40d04290b2938efeaa78d1c8a75c5d2506b236d9
URMUSEI <danceyouoff23@gmail.com>
2022-08-01 16:02:34 +0900

    for login

--- src/login.js
+++ src/login.js
@@ -1,82 +1,86 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import { Link, useNavigate } from "react-router-dom";
-import Button from "./Button";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import Button from './Button';
 
 function Login() {
-  const [inputId, setInputId] = useState("");
-  const [inputPw, setInputPw] = useState("");
+  const [inputId, setInputId] = useState('')
+  const [inputPw, setInputPw] = useState('')
 
   const loginId = {
     userid: inputId,
     userpw: inputPw,
-  };
-
-  const navigate = useNavigate();
-
-  //input data의 변화가 있을때마다 value값 useState화
+  }
+  
+    //input data의 변화가 있을때마다 value값 useState화
   const handleInputId = (e) => {
     setInputId(e.target.value);
-  };
+  }
 
   const handleInputPw = (e) => {
     setInputPw(e.target.value);
-  };
+  }
 
-  //Login 버튼 클릭 이벤트
+    //Login 버튼 클릭 이벤트
   const onClickLogin = () => {
-    if (loginId.userid === "" || loginId.userpw === "") {
-      alert("아이디 혹은 비밀번호를 기입하지 않으셨습니다.");
-    } else {
-      axios.post("/damlogin", loginId).then(function (res) {
-        if (res.data === true) {
-          navigate("/main", {state : loginId.userid});
-          console.log(res.data);
-        } else {
-          alert(res.data);
-        }
-      });
-    }
-  };
+    console.log('click login');
+    axios.post("/damlogin", loginId).then(function (res) {
+      console.log(res);
+    });
+  }
 
-  return (
+
+  return(
     <div id="logining" className="damlogin">
-      <h1>DAMTIME</h1>
-      <div className="damback">
-        <h2>Login</h2>
-        <form>
-          <label htmlFor="input_id">User : </label>
-          <input
-            type="email"
-            name="input_id"
-            value={inputId}
-            onChange={handleInputId}
-            placeholder="Type your e-mail"
-          />
-          <br />
-          <label htmlFor="input_pw">Pass : </label>
-          <input
-            type="password"
-            name="input_pw"
-            value={inputPw}
-            onChange={handleInputPw}
-            placeholder="Type your password"
-          />
-        </form>
+      <br/><h1>DAMTIME</h1>
+      <div className='damback2'>
+      <h2>Welcome Back!</h2>
+      <fieldset className='tit'>
+        
+      <form>
+      <legend className='tit2'>Log In</legend>
+        <label htmlFor='input_id'>User : </label>
+        <input 
+        type='email' 
+        name='input_id' 
+        value={inputId} 
+        onChange={handleInputId}
+        placeholder="Type your e-mail" />
         <br />
-        <br />
-        <button className="butto" onClick={onClickLogin} value="LOGIN">
-          login
-        </button>
+        <label htmlFor='input_pw'>Pass : </label>
+        <input 
+        type='password' 
+        name='input_pw' 
+        value={inputPw} 
+        onChange={handleInputPw}
+        placeholder="Type your password" />
+        <br /><br/>
+        <input 
+        className="butto" 
+        type="submit"
+        onClick={onClickLogin}
+        value="LOGIN"
+        ></input></form></fieldset>
+        <br/><br/><form>
         <Link to='/main'>
-        <button className="button">비회원으로 이용</button>
-        </Link>
+          <input type='button' className="text" value="Using for Non remember"></input>
+        </Link><br/>
         <Link to="/register/">
-          <Button className="text" name="회원가입"></Button>
+        <input type='button' className="text" value="Or Sign Up Using"></input>
         </Link>
-      </div>
+        {/* 구 회원가입 버튼
+        <input 
+        className="button" 
+        type="submit"
+        onClick={onClickRegister}
+        value="회원가입"
+        ></input> */}
+
+        </form>
+        </div>
     </div>
   );
+
 }
 
 export default Login;
