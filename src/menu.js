601c7986ae1be5ec358830ebd196ab4e3fb5ec4c
DevP3PP3R <petercater@naver.com>
2022-08-03 03:17:03 +0900

    햄버거 메뉴 만들어놓음

---
+++ src/menu.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { Spin as Hamburger } from 'hamburger-react'
+import "./menu.css"
+
+const Menu = () => {
+
+    const [isOpen, setOpen] = useState(false);
+  
+    return(
+    <div>
+    <h3>테스트중입니다</h3> 
+    <Hamburger size={20} direction="right" toggled={isOpen} toggle={setOpen} />        
+    
+    <ul className={isOpen ? "show-menu" : "hide-menu"}>
+            <li ><Link to="/">첫화면</Link></li>
+            <li ><Link to="/main">지도화면</Link></li>
+            <li >예시</li>
+            <li >앨랠래</li>
+    </ul>
+    </div>
+    );
+}
+export default Menu;
