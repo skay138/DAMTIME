174f07cffc6607d33d610af615acf6d39bb52e20
skay138 <nine1352@gmail.com>
2022-08-03 16:51:38 +0900

    1

--- src/Addfin.js
+++ src/Addfin.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import "./addfin.css";
 import Camera from "./camera";
