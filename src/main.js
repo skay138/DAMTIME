74fa83a2a7b21cd75a2d7eafd89d2088fd82316a
skay138 <nine1352@gmail.com>
2022-08-04 00:30:28 +0900

    123

--- src/main.js
+++ src/main.js
@@ -179,7 +179,7 @@ function Main() {
     var mapContainer = document.getElementById("myMap"), // 지도를 표시할 div
       mapOption = {
         center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
-        level: 4, // 지도의 확대 레벨
+        level: 3, // 지도의 확대 레벨
       };
 
     map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
