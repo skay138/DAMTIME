5c07dbdfb3e9fb9497a87e54a4a8b2d564248b9f
DevP3PP3R <petercater@naver.com>
2022-08-05 02:20:37 +0900

    흑흑

--- src/Myinfo.js
+++ src/Myinfo.js
@@ -1,12 +1,15 @@
 import Menu from "./menu";
 import React, { useEffect, useState } from "react";
 import DataTable from "./DataTable";
+import "./myinfo.css"
 
 function Myinfo() {
+  
   return (
     <div>
       <div className="title">DAMTIME</div>
       <Menu />
+      <h2 className="mypintitle">내 핀 관리</h2>
       <DataTable />
     </div>
   );
