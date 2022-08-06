64ccfbd1c80161d47790cb064f9b6abfdb595e09
skay138 <nine1352@gmail.com>
2022-08-06 20:21:46 +0900

    랜더링 문제 해결

--- src/DataTable.js
+++ src/DataTable.js
@@ -1,56 +1,61 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { DataGrid } from "@mui/x-data-grid";
 import axios from "axios";
 
-
 export default function DataTable() {
-
   const [selectionModel, setSelectionModel] = useState([]);
 
+  const userid = sessionStorage.getItem("loginId");
+
   const columns = [
-    { field: "id", headerName: "고유번호", width: 70 }, //No
+    { field: "id", headerName: "번호", width: 70 }, //No
     { field: "Location", headerName: "장소명", width: 400 },
     { field: "Description", headerName: "상세설명", width: 200 },
-    { field: "FacilityType", headerName: "장소유형", width: 150 }
+    { field: "FacilityType", headerName: "장소유형", width: 150 },
   ];
-  
+
   const [rows, setRows] = useState([]);
+
   const rowfn = (p) => {
-   Array.from(p).forEach(el => {
-    el.id = el.No;
-   });
-   setRows(p);
+    Array.from(p).forEach((el) => {
+      el.id = el.No;
+    });
+    setRows(p);
+  };
+
+  useEffect(() => {
+    axios
+      .get("https://damtime.kro.kr:4000/getmypin", { params: { userid } })
+      .then((res) => {
+        // console.log(userid)
+        // console.log(res.data);
+        const mypin = res.data;
+        rowfn(mypin);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  const handdlechange = (e) => {
+    setSelectionModel(e);
+  };
+  
+  const deletepin = () =>{
+    console.log(...selectionModel);
+
   }
 
-  const userid = sessionStorage.getItem("loginId");
-  axios
-    .get("https://damtime.kro.kr:4000/getmypin", {params: {userid}})
-    .then((res) => {
-      // console.log(userid)
-      // console.log(res.data);
-      const mypin = res.data;
-      rowfn(mypin);
-    })
-    .catch((error) => console.log(error));
-
-  
-
   return (
-    <div
-      style={{ position: "absolute", top: 150, height: 400, width: "100%" }}
-    >
+    <div style={{ position: "absolute", top: 150, height: 400, width: "100%" }}>
       <DataGrid
         rows={rows}
         columns={columns}
         pageSize={5}
         rowsPerPageOptions={[5]}
         checkboxSelection
-        onSelectionModelChange={(newSelectionModel) => {
-          setSelectionModel(newSelectionModel);
-          console.log(selectionModel);
-        }}
+        onSelectionModelChange={handdlechange}
         selectionModel={selectionModel}
       />
+      <button onClick={deletepin}>삭제</button>
     </div>
   );
 }
