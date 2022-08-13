d18e2794332b5e470759bb31fedb7080f7956253
skay138 <nine1352@gmail.com>
2022-08-13 20:23:20 +0900

    도움말 추가

--- src/help.js
+++ src/help.js
@@ -9,13 +9,13 @@ function Help() {
       <div className="title">DAMTIME</div>
       <Menu />
       <ul>
-        <li><img src="https://img.icons8.com/ios-filled/100/3498DB/marker-d.png" width="35px" alt="구"></img> : 구에서 인증한 공공 흡연구역</li>
+        <li><img src="https://img.icons8.com/ios-filled/100/3498DB/marker-d.png" width="35px" alt="구"></img> : 구에서 정보제공한 흡연구역</li>
         <li><img src="https://img.icons8.com/ios-filled/100/000000/marker-u.png" width="35px" alt="유저"></img> : 유저가 추가한 사설 흡연구역</li>
         <li><img src="https://img.icons8.com/sf-regular/96/000000/add.png" width="35px" alt="추가"></img> : 핀 등록버튼&gt;등록할위치 클릭</li>
-        <li><img src="https://img.icons8.com/ios-filled/50/000000/location-update.png" width="35px" alt="새로고침"></img> : 새로고침(현위치)</li>
+        <li><img src="https://img.icons8.com/ios-filled/50/000000/location-update.png" width="35px" alt="새로고침"></img> : 새로고침(현위치)<br/>현위치마커는 클릭하면 사라집니다.</li>
         <li>흡연구역 등록 및 수정요청은 로그인을<br/>하셔야 서비스 이용이 가능합니다.</li>
         <li>마커 클릭&gt;상세정보배너 클릭&gt;상세정보<br/>요약정보/맵 클릭&gt;요약정보끄기</li>
-        <li>등록하신 핀은 내정보에서 관리가능</li>
+        <li>본인이 등록한 마커는 내 정보 또는<br/>마커 정보에서 관리가 가능합니다.</li>
       </ul>
     </div>
   );
